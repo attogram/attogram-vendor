1ce3450bdf39ec3da7ee76f6de23eca31e56ca09
David B <barberi.office@gmail.com>
2016-05-21 12:05:24 +0200

    rm symfony/finder

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -15,7 +15,6 @@ class ComposerStaticInitb1d8e7be2253f2e605096846d109ac81
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\HttpFoundation\\' => 33,
-            'Symfony\\Component\\Finder\\' => 25,
         ),
         'M' => 
         array (
@@ -31,10 +30,6 @@ class ComposerStaticInitb1d8e7be2253f2e605096846d109ac81
         'Symfony\\Component\\HttpFoundation\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/http-foundation',
-        ),
-        'Symfony\\Component\\Finder\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/finder',
         ),
         'Monolog\\' => 
         array (
